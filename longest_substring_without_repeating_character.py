d2fb66e51309b96fa0dbadbd48276d043a3976ba
bingo_zhou <bingoabs@gmail.com>
2017-09-12 11:04:33 +0800

    longest substring

--- longest_substring_without_repeating_character.py
+++ longest_substring_without_repeating_character.py
@@ -2,7 +2,7 @@
 # @Author: bingo_zhou
 # @Date:   2017-09-11 16:38:42
 # @Last Modified by:   bingo_zhou
-# @Last Modified time: 2017-09-11 16:39:11
+# @Last Modified time: 2017-09-12 11:04:06
 
 """
 Examples:
@@ -14,8 +14,32 @@ Given "bbbbb", the answer is "b", with the length of 1.
 Given "pwwkew", the answer is "wke", with the length of 3. Note that the answer must be a substring, "pwke" is a subsequence and not a substring.
 """
 class Solution(object):
+    def get_start(self, string):
+        longest = ""
+        l = list(string)
+        while len(l) != len(set(l)):
+            half = len(l)/2
+            l = l[:half]
+        if len(l) < len(string):
+            for i in range(len(l), len(string)):
+                if len(string[:i]) == len(set(string[:i])):
+                    continue
+                return string[:i-1]
+        return l
+
     def lengthOfLongestSubstring(self, s):
         """
         :type s: str
         :rtype: int
         """
+        longest = ""
+        for i in range(len(s)):
+            new = self.get_start(s[i:])
+            if len(new) > len(longest):
+                longest = new
+        return len(longest)
+
+if __name__ == "__main__":
+    string = "abcajserwera"
+    r = Solution().lengthOfLongestSubstring(string)
+    print r
