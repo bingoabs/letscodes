c8d90173b6015b322179ac2bf56e389f3c17efd1
bingo_zhou <bingoabs@gmail.com>
2017-10-12 12:38:29 +0800

    is match issue

--- is_match.py
+++ is_match.py
@@ -19,62 +19,126 @@ isMatch('aa', 'a*') -> true
 isMatch('aa', '.*') -> true
 isMatch("ab", ".*") -> true
 isMatch("aab", "c*a*b") -> true
+
+maybe another way to do:
+    split the target string and goal string, find the match start and check the consistency with the left part
+    in fact, exactly same with the current method, but due tu the spilited, left work is just to compare, 
+    do not need to splite again and again...
+
+    first try solve the problem by opposite direction, so ...
 """
-import logging
+# import logging
+
+# class Solution(object):
+#     def isMatch(self, s, p):
+#         """
+#         :type s: str
+#         :type p: str
+#         :rtype: bool
+#         """
+#         if ".*" in p or s in p: return True 
+
+#         if len(s) == 0: return True
+#         if len(p) == 0: return False
+
+#         start = s[0]
+
+#         for i in range(len(p)):
+#             if p[i] in [start, "."]:
+#                 is_match = self.check(s, p[i:])
+#                 if is_match: return True
+#         return False
+
+#     def check(self, s, p):
+#         print(" s: {}; p: {}".format(s, p))
+#         if len(s) == 0: return True
+#         if len(p) == 0: return False
+
+#         if "*" in p:
+#             pre, last = p.split("*", 1)
+#             if self.same(pre, s[:len(pre)]):
+#                 left = self.subtract(s[len(pre):], pre[-1])
+#                 print("left: {}".format(left))
+#                 return self.check(left, last)
+#             else:
+#                 return False
+#         else:
+#             return self.same(s, p)
+
+#     def subtract(self, subtracted, subtraction):
+#         print("subtracted: {}; subtraction: {}".format(subtracted, subtraction))
+#         for i in range(len(subtracted)):
+#             if subtracted[i] != subtraction:
+#                 break
+#         return subtracted[i+1:]
+
+
+#     def same(self, expression, string):
+#         if len(string) < len(expression):return False
+#         for index in range(len(expression)):
+#             if expression[index] != string[index] and expression[index] != ".":
+#                 return False
+#         return True
 
 class Solution(object):
     def isMatch(self, s, p):
-        """
-        :type s: str
-        :type p: str
-        :rtype: bool
-        """
-        if ".*" in p or s in p: return True 
-
         if len(s) == 0: return True
         if len(p) == 0: return False
+        s_list = self.split(s)
+        p_list = self.split(p, re=True)
+        return self.comps(s_list, p_list)
 
-        start = s[0]
+    def split(self, s, re=False):
+        last, part, result = s[0], '', []
+        index, m_index = 0, len(s) - 1
+        while re == False and index <= m_index:
+            c_word = s[index]
+            if c_word == last:
+                part += c_word
+            else:
+                result.append(part)
+                part = c_word
+                last = c_word
+            index +=1 
 
-        for i in range(len(p)):
-            if p[i] in [start, "."]:
-                is_match = self.check(s, p[i:])
-                if is_match: return True
+        while re and index <= m_index:
+            c_word = s[index]
+            if c_word == last or c_word == "*":
+                part += c_word
+            else:
+                result.append(part)
+                part = c_word
+                last = c_word
+            index += 1
+        result.append(part)
+
+        return result
+
+    def comps(sl, pl):
+        start = sl[0]
+        for index in range(len(pl)):
+            if pl[index] in [start, ".", ".*"]:
+                if self.comp(sl, pl[index:]):
+                    return True
         return False
 
-    def check(self, s, p):
-        print(" s: {}; p: {}".format(s, p))
-        if len(s) == 0: return True
-        if len(p) == 0: return False
-
-        if "*" in p:
-            pre, last = p.split("*", 1)
-            if self.same(pre, s[:len(pre)]):
-                return False
-            left = self.subtract(s[len(pre):], pre[-1])
-            return self.check(left, last)
-        else:
-            pass
-
-    def subtract(self, subtracted, substraction):
-        for i in range(len(subtracted)):
-            if subtracted[i] != substraction:
-                break
-        return subtracted[i:]
-
-
-    def same(self, expression, string):
-        for index in range(len(expression)):
-            if expression[index] != string[index] and expression[index] != ".":
-                return False
+    def comp(sl, pl):
+        # todo just compare the start to end..
         return True
 
-        
-
+def test_split():
+    s = Solution()
+    print(s.split("ab"))
+    print(s.split(".*c", True))
+    # ['a', 'b']
+    # ['.*', 'c']
 
 if __name__ == "__main__":
-    s, p = "aa", "a*"
-    s = Solution().isMatch(s, p)
-    print(s)
+    # s, p = "ab", ".*c"
+    # s = Solution().isMatch(s, p)
+    # print(s)
+
+    test_split()
+
 
 
