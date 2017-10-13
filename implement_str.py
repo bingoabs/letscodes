85bd50c48eb6b82b37a9c06ff34b410cf78dd457
bingo_zhou <bingoabs@gmail.com>
2017-10-13 19:50:43 +0800

    add implement str script

---
+++ implement_str.py
@@ -0,0 +1,23 @@
+"""
+Implement strStr()
+Returns the index of the first occurrence of needle in haystack, or -1 if
+needle is not part of haystack.
+"""
+
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        pass
+
+
+
+
+if __name__ == "__main__":
+    haystack, needle = "", ""
+    s = Source().strStr(haystack, needle)
+    print(s)
+
